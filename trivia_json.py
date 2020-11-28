b1ece0f5afe9be817e192c519c2cc6c6a95c4c56
dianakakoma <dikakoma@gmail.com>
2020-11-28 16:44:13 -0600

    added the Tkinter submit button

--- trivia_json.py
+++ trivia_json.py
@@ -1,13 +1,23 @@
 from tkinter import *
+import json
 
 root = Tk()
 
+#clicked button function
+def myClick():
+    myLabel = Label(root, text="Look! I clicked the button!")
+    myLabel.grid(row=3, column=0)
+
 #creating lable widget
 myLabel1 = Label(root, text="Welcome to trivia!")
-myLabel2 = Label(root, text="My Name is Diana.")
+myLabel2 = Label(root, text="My name is Diana.")
+#question = Label(root, text=)
+submitButton = Button(root, text="submit your answer", command=myClick, bg="blue")
 
-#shoving it into the screen
+#placing elements into the screen
 myLabel1.grid(row=0, column=0)
 myLabel2.grid(row=1, column=0)
+
+submitButton.grid(row=2, column=0)
 
 root.mainloop()
